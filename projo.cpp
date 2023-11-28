e2b33a1aa0fa9a49a723843ca79e8bd80e914e7a
Mikołaj Data <miki3867@gmail.com>
2023-11-28 14:30:32 +0100

    Small refactor

--- projo.cpp
+++ projo.cpp
@@ -1,77 +1,76 @@
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 using namespace std;
-int main()
- {
+int main() {
 
-srand(static_cast<unsigned int>(time(0)));
-int N = rand() % 45 + 5;  
-int M = rand() % 45 + 5;
-int tab[N][M];
-  
-for (int i = 0; i < N; ++i) {
+  srand(static_cast<unsigned int>(time(0)));
+  int N = rand() % 45 + 5;
+  int M = rand() % 45 + 5;
+  int tab[N][M];
+
+  for (int i = 0; i < N; ++i) {
     for (int j = 0; j < M; ++j) {
-        tab[i][j] = rand() % 6;
-        if (tab[i][j]>1){
-            tab[i][j]=0;
-		}
+      tab[i][j] = rand() % 6;
+      if (tab[i][j] > 1) {
+        tab[i][j] = 0;
+      }
     }
-}
-int i, j, t;//t=liczba jedynek
+  }
+  int i, j, liczbaJedynek; // t=liczba jedynek
 
-
-	 for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            if (tab[i][j] == 1 ){
-            	if(tab[i][j-2] == 0 && tab[i][j-1] == 0 
-				&& tab[i-1][j-1] == 0 && tab[i+1][j-1] == 0
-				&& tab[i-1][j] == 0 && tab[i+1][j] == 0
-				&& tab[i-2][j] == 0 && tab[i+2][j] == 0 
-				&& tab[i-1][j+1] == 0 && tab[i+1][j+1] == 0
-				&& tab[i][j+1] == 0 && tab[i][j+2] == 0)
-			{
-            	if (i - 1 >= 0) tab[i - 1][j] = 1;  
-                if (i + 1 < N) tab[i + 1][j] = 1;   
-                if (j - 1 >= 0) tab[i][j - 1] = 1;  
-                if (j + 1 < M) tab[i][j + 1] = 1; 
-            }
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < M; ++j) {
+      if (tab[i][j] == 1) {
+        if (tab[i][j - 2] == 0 && tab[i][j - 1] == 0 &&
+            tab[i - 1][j - 1] == 0 && tab[i + 1][j - 1] == 0 &&
+            tab[i - 1][j] == 0 && tab[i + 1][j] == 0 && tab[i - 2][j] == 0 &&
+            tab[i + 2][j] == 0 && tab[i - 1][j + 1] == 0 &&
+            tab[i + 1][j + 1] == 0 && tab[i][j + 1] == 0 &&
+            tab[i][j + 2] == 0) {
+          if (i - 1 >= 0)
+            tab[i - 1][j] = 1;
+          if (i + 1 < N)
+            tab[i + 1][j] = 1;
+          if (j - 1 >= 0)
+            tab[i][j - 1] = 1;
+          if (j + 1 < M)
+            tab[i][j + 1] = 1;
         }
-	}
-}
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            if (tab[i][j] == 1) {
-                if (i+1<M && tab[i+1][j]==1){
-                	{ if (tab[i+1][j]==1 && tab[i][j-2] == 0 && tab[i][j-1] == 1 
-				&& tab[i-1][j-1] == 0 && tab[i+1][j-1] == 0
-				&& tab[i-1][j] == 1 && tab[i+1][j] == 1
-				&& tab[i-2][j] == 0 && tab[i+2][j] == 0 
-				&& tab[i-1][j+1] == 0 && tab[i+1][j+1] == 0
-				&& tab[i][j+1] == 1 && tab[i][j+2] == 0){
-				
-                			t++;
-						}
-					}
-				}
-            }
-        }
+      }
     }
+  }
 
-for(i=0;i<N;i++)
-	{
-		for(j=0;j<M;j++)
-		{
-			cout<<tab[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-cout<<endl<<"wielkosc tabeli NxM: "<<N<<"x"<<M;
-cout<<endl<<"znalezione plusy: "<<t;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < M; ++j) {
+      if (tab[i][j] == 1) {
+        if (i + 1 < M && tab[i + 1][j] == 1) {
+          {
+            if (tab[i + 1][j] == 1 && tab[i][j - 2] == 0 &&
+                tab[i][j - 1] == 1 && tab[i - 1][j - 1] == 0 &&
+                tab[i + 1][j - 1] == 0 && tab[i - 1][j] == 1 &&
+                tab[i + 1][j] == 1 && tab[i - 2][j] == 0 &&
+                tab[i + 2][j] == 0 && tab[i - 1][j + 1] == 0 &&
+                tab[i + 1][j + 1] == 0 && tab[i][j + 1] == 1 &&
+                tab[i][j + 2] == 0) {
 
+              liczbaJedynek++;
+            }
+          }
+        }
+      }
+    }
+  }
 
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      cout << tab[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << endl << "wielkosc tabeli NxM: " << N << "x" << M;
+  cout << endl << "znalezione plusy: " << liczbaJedynek;
 
- 	return 0;
- }
+  return 0;
+}
